8da04e1a842cb3af0fe52d095daf2353d41dd73c
root <root@ip-172-31-25-57.eu-central-1.compute.internal>
2024-02-15 00:55:11 +0000

    Web serwer

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -6,12 +6,10 @@ use App\Entity\Clients;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use function PHPUnit\Framework\at;
 
 class AdminDashboardController extends AbstractDashboardController
 {
--- src/Controller/Admin/ClientsCrudController.php
+++ src/Controller/Admin/ClientsCrudController.php
@@ -5,10 +5,8 @@ namespace App\Controller\Admin;
 use App\Entity\Clients;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use SebastianBergmann\CodeCoverage\Report\Text;
 
 
 class ClientsCrudController extends AbstractCrudController
